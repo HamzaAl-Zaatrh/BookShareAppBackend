f1f541fe2188dc28cf41a5204ff9ef7a3962ff08
HamzaAl-Zaatreh <hamzazaatreh20@gmail.com>
2023-03-07 19:58:36 +0300

    add some setting

--- BookShareBackend/BookShareBackend/settings.py
+++ BookShareBackend/BookShareBackend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y*&v5fkxlg5eex)6b*e5*65v2(fa=*r%1u00vw927*p6iqy+%l'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- BookShareBackend/core/models.py
+++ BookShareBackend/core/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.core import validators
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Books(models.Model):
+    book_name = models.CharField(max_length=200)
+    author = models.CharField(max_length=30)
+    publisher = models.CharField(max_length=30)
+    description = models.TextField(max_length=200)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    ISBN = models.IntegerField()
+    year = models.IntegerField()
+    avg_rating = models.FloatField(default=0)
+    number_rating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.book_name
+
+class Categories(models.Model):
+    pass
+
+class BookCategories(models.Model):
+    pass
+
+class UserBooks(models.Model):
+    pass
+
+class BookRatings(models.Model):
+    pass 
+
+#  
+  
+# class UserRatings(models.Model):
+#     pass
